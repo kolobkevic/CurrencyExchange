f34e6122ef2473e01fd4a9d5b1b681910034572f
Zaur Bogdaev <kolobkevic@gmail.com>
2024-04-28 00:44:13 +0300

    Change EncodingFilter and PathUtils

--- src/main/java/ru/kolobkevic/currencyexchange/common/EncodingFilter.java
+++ src/main/java/ru/kolobkevic/currencyexchange/common/EncodingFilter.java
@@ -14,8 +14,7 @@ public class EncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json;charset=UTF-8");
+        response.setContentType("application/json; charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
